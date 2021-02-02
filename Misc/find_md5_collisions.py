ea885a6e51fb85267aecf181428caa9ae018c510
Turodoras <turodoras@gmail.com>
2021-02-02 11:46:32 -0500

    [MISC] add more results cuz im bored

--- Misc/find_md5_collisions.py
+++ Misc/find_md5_collisions.py
@@ -1,14 +1,53 @@
 #!/usr/bin/env python3
 
-import hashlib
+from hashlib import md5
+import random
+import os
 
-string = b"b"
-count = 1
-while True:
-    h = hashlib.md5(string).digest()
-    if h[-2:] == b"AB":
-        print(f"[x] hash = {hashlib.md5(string).hexdigest()}")
-        print(f"[x] string = '{chr(string[0])}' * {count}")
-        break
-    string += b"a"
-    count += 1
+toFile = True
+if toFile:
+    os.system('rm -f logs.txt')
+
+def log(msg):
+    print(msg)
+    if toFile == True:
+        f = open("logs.txt", "a")
+        f.write(msg + '\n')
+        f.close()
+
+def isGood(h):
+    if h.digest()[-2:] == b"AB":
+        return True
+    return False
+
+def findString():
+    string = b"b"
+    count = 1
+    while True:
+        h = md5(string)
+        if isGood(h):
+            log(f"[x] hash = {h.hexdigest()}, string = '{chr(string[0])}' * {count}")
+            break
+        string += bytes([string[0]])
+        count += 1
+
+def findNumber():
+    number = 0
+    while True:
+        h = md5(str(number).encode('utf-8'))
+        if isGood(h):
+            log(f"[x] hash = {h.hexdigest()}, number = {number}")
+            break
+        number += 1
+
+def findFile(filename):
+    f = open(filename, errors='ignore')
+    for line in f.readlines():
+        l = line[:-1]
+        h = md5(l.encode('utf-8'))
+        if isGood(h):
+            log(f"[x] hash = {h.hexdigest()}, string = '{l}'")
+
+findString()
+findNumber()
+findFile('/usr/share/wordlists/rockyou.txt')
